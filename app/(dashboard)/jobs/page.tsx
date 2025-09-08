4098bcbca78c716036d9c530272a33a064eea896
Vidyasagar Akhumukhi <sagar112113@gmail.com>
2025-09-08 17:33:01 +0100

    homepage layout

---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobsPage = () => {
+  return <h1 className="text-5xl">Jobs page</h1>;
+};
+
+export default JobsPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,43 @@
 import { Button } from "@/components/ui/button";
 import { Camera } from "lucide-react";
+import Image from "next/image";
+import Logo from "../assets/logoFront.png";
+import LandingImg from "../assets/jobhunt.svg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1>
-        <Button>Default button</Button>
-        <Button variant="destructive" size="lg">
-          <Camera />
-        </Button>
-      </h1>
-    </div>
+    <main>
+      <header className="max-w-6xl px-4 sm:px-8 py-6 ">
+        <Image src={Logo} alt="logo" />
+      </header>
+      <section className="max-w-8xl mx-auto px-4 sm:px-8 h-20 -mt-10 grid lg:grid-cols-2 items-center gap-5">
+        <div className="items-center mx-auto">
+          <h1 className="capitalize text-5xl md:text-8xl font-bold">
+            job <span className="text-primary">tracking</span> app
+          </h1>
+          <p className="leading-loose max-w-4xl mt-4 ">
+            I am baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          <Button asChild className="mt-4">
+            <Link href="/add-job">Get Started</Link>
+          </Button>
+        </div>
+
+        <div className="hidden lg:block mx-auto">
+          <Image
+            src={LandingImg}
+            alt="landing"
+            // Set a fixed width and height here
+            width={500}
+            height={500}
+            priority
+          />
+        </div>
+      </section>
+    </main>
   );
 }
