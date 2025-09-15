c85e8488aa832c511506ecd8536c04695f84c71f
Vidyasagar Akhumukhi <sagar112113@gmail.com>
2025-09-15 16:20:57 +0100

    Remove unused pageButtons variable in ButtonContainer
    Eliminated the unused 'pageButtons' array from ComplexButtonContainer.tsx to clean up the code and improve maintainability.

--- components/ComplexButtonContainer.tsx
+++ components/ComplexButtonContainer.tsx
@@ -18,8 +18,6 @@ function ButtonContainer({ currentPage, totalPages }: ButtonContainerProps) {
   const router = useRouter();
   const pathname = usePathname();
 
-  const pageButtons = Array.from({ length: totalPages }, (_, i) => i + 1);
-
   const handlePageChange = (page: number) => {
     const defaultParams = {
       search: searchParams.get("search") || "",
@@ -27,7 +25,7 @@ function ButtonContainer({ currentPage, totalPages }: ButtonContainerProps) {
       page: String(page),
     };
 
-    let params = new URLSearchParams(defaultParams);
+    const params = new URLSearchParams(defaultParams);
 
     router.push(`${pathname}?${params.toString()}`);
   };
